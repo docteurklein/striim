1b0406540db9dcc57b37ac94ae242ad074de8897
Klein Florian <florian.klein@free.fr>
2011-11-18 11:02:50 +0100

    dont hardocd video src

--- vod.js
+++ vod.js
@@ -5,25 +5,30 @@
         i.src = 'http://'+ window.location.hostname +':8081/requests/vlm_cmd.xml?command='+encodeURIComponent(action);
     };
 
+    var getMediaName = function() {
+        return window.location.hash.replace('#', '');
+    };
+
     document.addEventListener('DOMContentLoaded', function () {
         player = document.getElementById('video-js');
-        call('setup VFV disabled');
-        call('setup VFV enabled');
+        call('setup '+ getMediaName() +' disabled');
+        call('setup '+ getMediaName() +' enabled');
+
+        player.src = 'http://'+ window.location.hostname +':8090/stream.ogg';
+        player.load();
 
         player.addEventListener('play', function() {
             //call('setup VFV enabled');
-            call('control '+ window.location.hash +' play');
-            //player.getElementsByTagName('source')[0].src = player.getElementsByTagName('source')[0].src;
-            //player.load();
+            call('control '+ getMediaName() +' play');
         });
 
         player.addEventListener('pause', function() {
-            call('control '+ window.location.hash +' pause');
+            call('control '+ getMediaName() +' pause');
         });
 
         player.addEventListener('seeking', function(event) {
             //console.log(arguments);
-            call('control '+ window.location.hash +' seek + 3s');
+            call('control '+ getMediaName() +' seek + 3s');
         });
      });
 }());
