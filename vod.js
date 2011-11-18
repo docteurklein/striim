0812e1133bd658b31d903bea879f60ea1da46a9c
Klein Florian <florian.klein@free.fr>
2011-11-18 10:22:57 +0100

    removed useless videojs

--- vod.js
+++ vod.js
@@ -2,31 +2,28 @@
     var call = function(action) {
         console.log(action);
         var i = document.createElement('img');
-        i.src = 'http://'+window.location.hostname+':8081/requests/vlm_cmd.xml?command='+encodeURIComponent(action);
-        //console.log(i.src);
+        i.src = 'http://'+ window.location.hostname +':8081/requests/vlm_cmd.xml?command='+encodeURIComponent(action);
     };
 
     document.addEventListener('DOMContentLoaded', function () {
         player = document.getElementById('video-js');
-        player.addEventListener('loadstart', function() {
-            //call('setup VFV disabled');
-            //call('setup VFV enabled');
-        });
+        call('setup VFV disabled');
+        call('setup VFV enabled');
 
         player.addEventListener('play', function() {
             //call('setup VFV enabled');
-            call('control VFV play');
+            call('control '+ window.location.hash +' play');
             //player.getElementsByTagName('source')[0].src = player.getElementsByTagName('source')[0].src;
             //player.load();
         });
 
         player.addEventListener('pause', function() {
-            call('control VFV pause');
+            call('control '+ window.location.hash +' pause');
         });
 
         player.addEventListener('seeking', function(event) {
-            console.log(arguments);
-            call('control VFV seek + 3s');
+            //console.log(arguments);
+            call('control '+ window.location.hash +' seek + 3s');
         });
      });
 }());
